88f2343373e2c2c8cc9b35bb06d9c6644a87b8d4
佐藤昭文 <akfm.sato@gmail.com>
2023-08-26 21:50:14 +0900

    add pages example

--- apps/example-next/next.config.js
+++ apps/example-next/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    scrollRestoration: true,
+  },
+};
 
 module.exports = nextConfig;
---
+++ apps/example-next/src/pages/pages/ssr/[id].tsx
@@ -0,0 +1,40 @@
+import { Counter } from "@/components/Counter";
+import { List } from "@/components/List";
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+
+type Props = {
+  id: number;
+};
+
+export default function Page({ id }: Props) {
+  const nextUrl = `/pages/ssr/${id + 1}`;
+  return (
+    <div>
+      <h1>SSR Page</h1>
+      <p>id: {id}</p>
+      <p>
+        <Link href="/pages">/pages</Link>
+      </p>
+      <p>
+        <Link href={nextUrl}>{nextUrl}</Link>
+      </p>
+      <Counter storeName="session" />
+      <Counter storeName="url" />
+      <List storeName="session" />
+      <List storeName="url" />
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<
+  Props,
+  { id: string }
+> = async ({ params }) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return {
+    props: {
+      id: Number(params?.id),
+    },
+  };
+};
