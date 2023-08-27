590b4a4c93a87e21dde604efdb71bce941b40ce9
佐藤昭文 <akfm.sato@gmail.com>
2023-08-27 17:23:03 +0900

    generate .d.ts, add exports field on package

--- apps/example-next/src/app/_components/Providers.tsx
+++ apps/example-next/src/app/_components/Providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { LocationStateProvider, NavigationSyncer } from "@location-state/core";
-import unsafeNavigation from "@location-state/unsafe-navigation";
+import { unsafeNavigation } from "@location-state/core/unsafe-navigation";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
