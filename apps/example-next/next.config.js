9268e18c8e3b788bdc47f18aa61ff74838dbf508
koichik <koichik@improvement.jp>
2023-09-09 23:49:34 +0900

    add launch.json for VSC and support for sourcemap

--- apps/example-next/next.config.js
+++ apps/example-next/next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
+
+  productionBrowserSourceMaps: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.devtool = "source-map";
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
