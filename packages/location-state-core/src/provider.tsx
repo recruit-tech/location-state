9019ea42eb9f6904e6552c2e8b43a987dab1d509
akfm <akfm.sato@gmail.com>
2023-09-15 21:01:04 +0900

    add stores props in LocationStateProvider (#13)
    * add stores props in LocationStateProvider
    
    * support create stores function props on Provider
    
    * fix context re render
    
    * export Stores,CreateStores,createDefaultStores
    
    ---------
    
    Co-authored-by: akfm.sato <01047245@CF0286.local>

--- packages/location-state-core/src/provider.tsx
+++ packages/location-state-core/src/provider.tsx
@@ -1,33 +1,41 @@
 import { LocationStateContext } from "./context";
 import { StorageStore, Store, URLStore } from "./stores";
 import { NavigationSyncer } from "./syncers";
-
 import { Syncer } from "./types";
 import { ReactNode, useEffect, useState } from "react";
+
+export type Stores = Record<string, Store>;
+export type CreateStores = (syncer: Syncer) => Stores;
+
+export const createDefaultStores: CreateStores = (syncer) => ({
+  session: new StorageStore(globalThis.sessionStorage),
+  url: new URLStore(syncer),
+});
 
 export function LocationStateProvider({
   children,
   ...props
 }: {
   syncer?: Syncer;
+  stores?: Stores | CreateStores;
   children: ReactNode;
 }) {
   const [syncer] = useState(
     () => props.syncer ?? new NavigationSyncer(window.navigation),
   );
-  const [contextValue] = useState(() => ({
-    stores: {
-      session: new StorageStore(globalThis.sessionStorage),
-      url: new URLStore(syncer),
-    },
-  }));
+  // Generated on first render to prevent provider from re-rendering
+  const [contextValue] = useState(() => {
+    const stores = props.stores ?? createDefaultStores;
+    return {
+      stores: typeof stores === "function" ? stores(syncer) : stores,
+    };
+  });
 
   useEffect(() => {
-    const stores = contextValue.stores;
     const abortController = new AbortController();
     const { signal } = abortController;
     const applyAllStore = (callback: (store: Store) => void) => {
-      Object.values(stores).forEach(callback);
+      Object.values(contextValue.stores).forEach(callback);
     };
 
     const key = syncer.key()!;
