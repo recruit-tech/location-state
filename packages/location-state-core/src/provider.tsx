1f76a84252a3d1765d1d405c23204ea67b8f6337
akfm.sato <01047245@CF0286.local>
2023-09-15 10:18:07 +0900

    support create stores function props on Provider

--- packages/location-state-core/src/provider.tsx
+++ packages/location-state-core/src/provider.tsx
@@ -1,30 +1,38 @@
 import { LocationStateContext } from "./context";
 import { StorageStore, Store, URLStore } from "./stores";
 import { NavigationSyncer } from "./syncers";
-
 import { Syncer } from "./types";
 import { ReactNode, useEffect, useState } from "react";
+
+type Stores = Record<string, Store>;
+type CreateStores = (syncer: Syncer) => Stores;
+
+const createDefaultStores: CreateStores = (syncer) => ({
+  session: new StorageStore(globalThis.sessionStorage),
+  url: new URLStore(syncer),
+});
 
 export function LocationStateProvider({
   children,
   ...props
 }: {
   syncer?: Syncer;
-  stores?: Record<string, Store>;
+  stores?: Stores | CreateStores;
   children: ReactNode;
 }) {
   const [syncer] = useState(
     () => props.syncer ?? new NavigationSyncer(window.navigation),
   );
-  const [contextValue] = useState(() => ({
-    stores: props.stores ?? {
-      session: new StorageStore(globalThis.sessionStorage),
-      url: new URLStore(syncer),
-    },
-  }));
+  const [stores] = useState(() => {
+    if (props.stores) {
+      return typeof props.stores === "function"
+        ? props.stores(syncer)
+        : props.stores;
+    }
+    return createDefaultStores(syncer);
+  });
 
   useEffect(() => {
-    const stores = contextValue.stores;
     const abortController = new AbortController();
     const { signal } = abortController;
     const applyAllStore = (callback: (store: Store) => void) => {
@@ -52,10 +60,10 @@ export function LocationStateProvider({
     );
 
     return () => abortController.abort();
-  }, [syncer, contextValue.stores]);
+  }, [syncer, stores]);
 
   return (
-    <LocationStateContext.Provider value={contextValue}>
+    <LocationStateContext.Provider value={{ stores }}>
       {children}
     </LocationStateContext.Provider>
   );
