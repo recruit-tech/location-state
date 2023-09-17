0b9caaacbe426cf7c8e24d4df12f5c7f0ee89d63
佐藤昭文 <akfm.sato@gmail.com>
2023-09-17 21:17:33 +0900

    move to playwrihght dir

--- apps/example-next/playwright.config.ts
+++ apps/example-next/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   // Look for test files in the "tests" directory, relative to this configuration file.
-  testDir: "tests",
+  testDir: "playwright",
   // Run all tests in parallel.
   fullyParallel: true, // Fail the build on CI if you accidentally left test.only in the source code.
   forbidOnly: !!process.env.CI, // Retry on CI only.
