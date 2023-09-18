2409a7374178e6f319b56d8a551132ebd28f53f8
佐藤昭文 <akfm.sato@gmail.com>
2023-09-18 17:31:49 +0900

    replace npx to pnpm dlx

--- apps/example-next/playwright/session-counter.spec.ts
+++ apps/example-next/playwright/session-counter.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { describe } from "node:test";
 
-// Safari/Firefox: different from actual movement
+// Safari/Firefox: different from actual behavior
 test.skip(({ browserName }) => browserName !== "chromium", "Chromium only!");
 
 describe('"session counter" is restored on browser back.', () => {
