f84b0024292969ce717d0823db5f627fcac0291c
佐藤昭文 <akfm.sato@gmail.com>
2023-09-21 21:25:42 +0900

    rm esm file export

--- packages/location-state-core/tsup.config.ts
+++ packages/location-state-core/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   target: "es2020",
-  format: ["cjs", "esm"],
+  format: ["cjs"],
   clean: true,
   // CI上ではdts生成より先にbuildが進んでしまうため、以下のissue解消後有効化
   // https://github.com/egoist/tsup/issues/921
