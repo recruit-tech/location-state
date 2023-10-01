5ac36e8f3f0c81ca4b8b7bb4097b75b915315b38
佐藤昭文 <akfm.sato@gmail.com>
2023-10-01 16:42:28 +0900

    fix integration test comment

--- apps/example-next-unsafe-navigation/playwright/session-counter.spec.ts
+++ apps/example-next-unsafe-navigation/playwright/session-counter.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { describe } from "node:test";
 
-// Safari/Firefox: not implement Navigation API
+// Safari/Firefox: different from actual behavior
 test.skip(({ browserName }) => browserName !== "chromium", "Chromium only!");
 
 describe('"session counter" is restored on browser back.', () => {
