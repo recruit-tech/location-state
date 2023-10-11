8e8bf8b54d04b6a2d420687bc17516b087e5e09e
佐藤昭文 <akfm.sato@gmail.com>
2023-10-11 23:13:52 +0900

    refactor: EventEmiiter impl

--- packages/location-state-core/src/stores/in-memory-store.ts
+++ packages/location-state-core/src/stores/in-memory-store.ts
@@ -1,3 +1,4 @@
+import { EventEmitter } from "./event-emitter";
 import { Listener, Store } from "./types";
 
 type State = Record<string, unknown>;
@@ -5,35 +6,12 @@ type State = Record<string, unknown>;
 export class InMemoryStore implements Store {
   private state: Record<string, unknown> = {};
   private storage: Map<string, State> = new Map();
-  private readonly listeners: Map<string, Set<Listener>> = new Map();
+  private eventEmitter = new EventEmitter();
   private currentKey: string | null = null;
 
   subscribe(name: string, listener: Listener) {
-    const listeners = this.listeners.get(name);
-    if (listeners) {
-      listeners.add(listener);
-    } else {
-      this.listeners.set(name, new Set([listener]));
-    }
-    return () => this.unsubscribe(name, listener);
-  }
-
-  private unsubscribe(name: string, listener: Listener) {
-    const listeners = this.listeners.get(name);
-    listeners?.delete(listener);
-    if (listeners?.size === 0) {
-      this.listeners.delete(name);
-    }
-  }
-
-  private notify(name: string) {
-    this.listeners.get(name)?.forEach((listener) => listener());
-  }
-
-  private notifyAll() {
-    this.listeners.forEach((listeners) =>
-      listeners.forEach((listener) => listener()),
-    );
+    this.eventEmitter.on(name, listener);
+    return () => this.eventEmitter.off(name, listener);
   }
 
   get(name: string) {
@@ -46,14 +24,14 @@ export class InMemoryStore implements Store {
     } else {
       this.state[name] = value;
     }
-    this.notify(name);
+    this.eventEmitter.emit(name);
   }
 
   load(locationKey: string) {
     if (this.currentKey === locationKey) return;
     this.currentKey = locationKey;
     this.state = this.storage.get(locationKey) ?? {};
-    queueMicrotask(() => this.notifyAll());
+    queueMicrotask(() => this.eventEmitter.emitAll());
   }
 
   save() {
