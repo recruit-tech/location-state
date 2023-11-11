4652ea02d321f6e603348bc26f173466b7a0dcc4
佐藤昭文 <akfm.sato@gmail.com>
2023-11-11 17:55:00 +0900

    example of react-hook-form init

---
+++ apps/example-react-hook-form/src/pages/index.tsx
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+
+type Form = {
+  firstName: string;
+  lastName: string;
+};
+
+export default function Home() {
+  const { register, handleSubmit } = useForm<Form>();
+  const router = useRouter();
+  const onSubmit = async (data: Form) => {
+    await router.push(
+      `/success?firstName=${data.firstName}&lastName=${data.lastName}`,
+    );
+  };
+
+  return (
+    <>
+      <Head>
+        <title>`react-hook-form` example</title>
+      </Head>
+      <main>
+        <h1>`react-hook-form` example</h1>
+        <h2>My form</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label>
+              first name:&nbsp;
+              <input type="text" {...register("firstName")} />
+            </label>
+          </div>
+          <div>
+            <label>
+              last name:&nbsp;
+              <input type="text" {...register("lastName")} />
+            </label>
+          </div>
+          <button>submit</button>
+        </form>
+      </main>
+    </>
+  );
+}
