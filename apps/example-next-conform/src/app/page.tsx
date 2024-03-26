1bdd0bf5a26e5372b01ec3ef27c959cd841d4ba7
akfm <akfm.sato@gmail.com>
2024-03-26 15:53:05 +0900

    examples: confrom init (#279)
    * examples: confrom init
    
    * example: conform example: use src directory, add next.config.js
    
    * example: conform example's remove button not working
    
    ---------
    
    Co-authored-by: akfm.sato <01047245@CF0286.local>

---
+++ apps/example-next-conform/src/app/page.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "`conform` example",
+};
+
+export default function Home() {
+  return (
+    <main>
+      <h1>`conform` example</h1>
+      <ul>
+        <li>
+          <Link href="/simple-form">simple-form</Link>
+        </li>
+        <li>
+          <Link href="/dynamic-form">dynamic-form</Link>
+        </li>
+      </ul>
+    </main>
+  );
+}
