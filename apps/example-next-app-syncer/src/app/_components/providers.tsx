00e0b064b3d49a8a6b80d55cf4d3b86999b38b59
AkifumiSato <akfm.sato@gmail.com>
2026-03-13 16:46:36 +0900

    fix: webkit with navigation api break test

--- apps/example-next-app-syncer/src/app/_components/providers.tsx
+++ apps/example-next-app-syncer/src/app/_components/providers.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { LocationStateProvider } from "@location-state/core";
+import { unsafeNavigation } from "@location-state/core/unsafe-navigation";
 import { useNextAppSyncer } from "@location-state/next";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const syncer = useNextAppSyncer();
+  const syncer = useNextAppSyncer({ navigation: unsafeNavigation });
   return (
     <LocationStateProvider syncer={syncer}>{children}</LocationStateProvider>
   );
