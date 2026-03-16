ba7aa6499c0ba1e1f988ea79982fc464e1d94b46
AkifumiSato <akfm.sato@gmail.com>
2026-03-16 19:09:41 +0900

    example: example-next-basicでuseNextAppSyncerを利用

--- apps/example-next-basic/src/app/_components/providers.tsx
+++ apps/example-next-basic/src/app/_components/providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { LocationStateProvider } from "@location-state/core";
+import { unsafeNavigation } from "@location-state/core/unsafe-navigation";
+import { useNextAppSyncer } from "@location-state/next";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <LocationStateProvider>{children}</LocationStateProvider>;
+  const syncer = useNextAppSyncer({ navigation: unsafeNavigation });
+  return (
+    <LocationStateProvider syncer={syncer}>{children}</LocationStateProvider>
+  );
 }
