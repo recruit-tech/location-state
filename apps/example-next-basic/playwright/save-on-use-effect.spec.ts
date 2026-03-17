6325a8eb84384af4bdd4faa59df3930037b2d5dd
AkifumiSato <akfm.sato@gmail.com>
2026-03-17 23:30:13 +0900

    fix: h1取得タイミングに依存するflaky

--- apps/example-next-basic/playwright/save-on-use-effect.spec.ts
+++ apps/example-next-basic/playwright/save-on-use-effect.spec.ts
@@ -4,11 +4,11 @@ test("restore random id that save on `useEffect()`", async ({ page }) => {
   // Navigate to the target page.
   await page.goto("http://localhost:3000/pages/save-on-use-effect");
   const h1Element = page.getByRole("heading", { level: 1 });
-  const initialH1Text = await h1Element.textContent();
-  await expect(initialH1Text).not.toBeNull();
   await expect(h1Element).toHaveText(
     /Save on `useEffect\(\)`, random value: \d+/,
   );
+  // Capture the text after useEffect has set the random value.
+  const initialH1Text = await h1Element.textContent();
   // Navigate to the top page.
   await page.getByRole("link", { name: /^\/pages$/ }).click();
   await expect(page).toHaveURL("http://localhost:3000/pages");
@@ -17,7 +17,7 @@ test("restore random id that save on `useEffect()`", async ({ page }) => {
   await expect(page).toHaveURL(
     "http://localhost:3000/pages/save-on-use-effect",
   );
-  await expect(
-    await page.getByRole("heading", { level: 1 }).textContent(),
-  ).toBe(initialH1Text);
+  await expect(page.getByRole("heading", { level: 1 })).toHaveText(
+    initialH1Text!,
+  );
 });
